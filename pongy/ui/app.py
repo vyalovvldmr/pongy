8b85285dd0808f5b3268f42fd4f326c30aecb837
Vladimir Vyalov <vyalov.v@gmail.com>
2022-09-30 07:52:22 +0300

    Add Connection class

--- pongy/ui/app.py
+++ pongy/ui/app.py
@@ -5,6 +5,7 @@ import threading
 import time
 import uuid
 from contextlib import suppress
+from typing import Protocol
 
 import aiohttp
 import pygame
@@ -31,55 +32,79 @@ class Exit:
     pass
 
 
-async def _process_output_queue(ws, output_queue):
-    while True:
-        await asyncio.sleep(0.01)
-        with suppress(queue.Empty):
-            await ws.send_json(output_queue.get_nowait().dict())
+class IApplication(Protocol):
+    host: str
+    port: int
+    input_queue: queue.Queue
+    output_queue: queue.Queue
 
 
-async def _handle(input_queue, output_queue, host, port):
-    player_id: str = str(uuid.uuid4())
-    url = f"ws://{host}:{port}/ws"
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.ws_connect(
-                url,
-                heartbeat=settings.WS_HEARTBEAT_TIMEOUT,
-                headers={"Cookie": f"player_id={player_id}"},
-            ) as ws:
-                asyncio.ensure_future(_process_output_queue(ws, output_queue))
-                async for msg in ws:
-                    if msg.type == aiohttp.WSMsgType.TEXT:
-                        ws_event = WsEvent.parse_raw(msg.data)
-                        input_queue.put(ws_event)
-    except aiohttp.ClientConnectionError:
-        logger.error("Connection error")
-        input_queue.put(Exit())
+class Connection(threading.Thread):
+    def __init__(self, app: IApplication):
+        super().__init__(daemon=True)
+        self._app = app
 
+    def run(self):
+        self._ws_connection()
 
-def ws_connection(input_queue, output_queue, host, port):
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(_handle(input_queue, output_queue, host, port))
+    def connect(self):
+        self.start()
+
+    async def _process_output_queue(self, ws):
+        while True:
+            await asyncio.sleep(0.01)
+            with suppress(queue.Empty):
+                await ws.send_json(self._app.output_queue.get_nowait().dict())
+
+    async def _handle(self):
+        player_id: str = str(uuid.uuid4())
+        url = f"ws://{self._app.host}:{self._app.port}/ws"
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.ws_connect(
+                    url,
+                    heartbeat=settings.WS_HEARTBEAT_TIMEOUT,
+                    headers={"Cookie": f"player_id={player_id}"},
+                ) as ws:
+                    asyncio.ensure_future(self._process_output_queue(ws))
+                    async for msg in ws:
+                        if msg.type == aiohttp.WSMsgType.TEXT:
+                            ws_event = WsEvent.parse_raw(msg.data)
+                            self._app.input_queue.put(ws_event)
+        except aiohttp.ClientConnectionError:
+            logger.error("Connection error")
+            self._app.input_queue.put(Exit())
+
+    def _ws_connection(self):
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(self._handle())
 
 
 class Application:
     def __init__(self, host: str, port: int):
         self._quit: bool = False
-        self._host: str = host
-        self._port: int = port
-        self._input_queue: queue.Queue = queue.Queue()
-        self._output_queue: queue.Queue = queue.Queue()
+        self.host: str = host
+        self.port: int = port
+        self.input_queue: queue.Queue = queue.Queue()
+        self.output_queue: queue.Queue = queue.Queue()
 
-    def connect(self):
-        keep_connection = threading.Thread(
-            target=ws_connection,
-            args=(self._input_queue, self._output_queue, self._host, self._port),
-            daemon=True,
-        )
-        keep_connection.start()
+    def run(self):
+        self._connect()
+        pygame.init()
+        surface = pygame.display.set_mode((settings.BOARD_SIZE, settings.BOARD_SIZE))
+        surface.fill(settings.BOARD_COLOR)
+        while not self._quit:
+            time.sleep(0.01)
+            self._process_input_queue(surface)
+            self._process_pygame_events()
+            self._process_key_pressed()
+        pygame.quit()
 
-    def draw_field(self, surface, ws_event):
+    def _connect(self):
+        connection = Connection(self)
+        connection.connect()
+
+    def _redraw_field(self, surface, ws_event):
         surface.fill(settings.BOARD_COLOR)
         for player, racket_widget, score_widget in zip(
             ws_event.data.payload.players,
@@ -93,7 +118,7 @@ class Application:
         BallWidget(ball_position).draw(surface)
         pygame.display.flip()
 
-    def process_pygame_events(self):
+    def _process_pygame_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._quit = True
@@ -101,35 +126,23 @@ class Application:
                 if event.key == pygame.K_q:
                     self._quit = True
 
-    def process_key_pressed(self):
+    def _process_key_pressed(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_UP]:
             ws_event = WsCommand(
                 payload=WsCommandMovePayload(direction=MoveDirection.LEFT)
             )
-            self._output_queue.put(ws_event)
+            self.output_queue.put(ws_event)
         if keys[pygame.K_RIGHT] or keys[pygame.K_DOWN]:
             ws_event = WsCommand(
                 payload=WsCommandMovePayload(direction=MoveDirection.RIGHT)
             )
-            self._output_queue.put(ws_event)
+            self.output_queue.put(ws_event)
 
-    def process_input_queue(self, surface):
-        while not self._input_queue.empty():
-            ws_event = self._input_queue.get_nowait()
+    def _process_input_queue(self, surface):
+        while not self.input_queue.empty():
+            ws_event = self.input_queue.get_nowait()
             if isinstance(ws_event, Exit):
                 self._quit = True
                 break
-            self.draw_field(surface, ws_event)
-
-    def run(self):
-        self.connect()
-        pygame.init()
-        surface = pygame.display.set_mode((settings.BOARD_SIZE, settings.BOARD_SIZE))
-        surface.fill(settings.BOARD_COLOR)
-        while not self._quit:
-            time.sleep(0.01)
-            self.process_input_queue(surface)
-            self.process_pygame_events()
-            self.process_key_pressed()
-        pygame.quit()
+            self._redraw_field(surface, ws_event)
